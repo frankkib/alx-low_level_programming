f425500e68ba66842dc73cec3cafa237832a1460
frankkib <frankkibagendi13@gmail.com>
2022-10-14 11:01:49 +0000

    finding the size of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * return: 0
+ */
+int main(void)
+{
+	int intergertype;
+	float floattpye;
+	double doubletpye;
+	char chartype;
+
+	printf("int size: %ld bytes\n", (sizeof(integertype));
+	printf("float size: %ld bytes\n", sizeof(floattype));
+	printf("double size: %ld bytes\n", sizeof(doubletype));
+	printf("char size: %ldbyte\n", sizeof(chartype));
+	return (0);
+}

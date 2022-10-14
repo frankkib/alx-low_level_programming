a9d4657eb8c61e7849a893c383a28323f7bc51d4
frankkib <frankkibagendi13@gmail.com>
2022-10-14 10:21:58 +0000

    printing simple line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - entry piont 
+ *  main-entry piont
  * Return: 0
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

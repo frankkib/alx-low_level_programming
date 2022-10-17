54539d105e46a91f6c70e9ebed2414f96f7f949d
frankkib <frankkibagendi13@gmail.com>
2022-10-17 09:40:35 +0000

    printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char aphabet = 'a';
+
+	for (aphabet ; aphabet <= z; aphabet++)
+	{
+		putchar(aphabet);
+	}
+	putchar('\n');
+}

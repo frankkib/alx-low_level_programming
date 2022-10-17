b197879480de68d44fd2d6a053bd31593a9e2776
frankkib <frankkibagendi13@gmail.com>
2022-10-17 11:13:33 +0000

    random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+/* betty style doc for function main goes there */
 /**
  * main - the main entry
- * description - positive or negative rondom number generated
- * return (0)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -13,11 +14,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+	{
 		printf("%i is positive\n", n);
+	}
 	else if (n < 0)
+	{
 	printf("%i is negative\n", n);
+	}
 	else
+	{
 	printf("%i is zero\n", n);
+	}
 	return (0);
 }
 

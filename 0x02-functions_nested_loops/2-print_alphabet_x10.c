2677b4c81fc5139fc78891c2f28acee0d2334f73
frankkib <frankkibagendi13@gmail.com>
2022-10-18 13:06:48 +0000

    print aphabets tens times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ * Description: Prints the alphabet with _putchar ten times
+ *  Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n')
+	}
+}

1e7b2578fc4ba822ca82b1f87d9d32d532cf8505
frankkib <frankkibagendi13@gmail.com>
2022-10-18 12:08:07 +0000

    printing putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i[8] = "_putchar"
+
+		int j;
+	for (j = 0; j < 8; j++)
+
+		_putchar(i[j]);
+
+	_putchar(8);
+	return (0);
+}

cd5fcfb08eda0293c888094627fcace20e42b427
frankkib <frankkibagendi13@gmail.com>
2022-10-19 11:35:50 +0000

    printing tables nine times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - To print out nine times tables
+ * Return: 0
+ */
+void times_table(void)
+{
+	int rows, columns, times;
+
+	for (rows = 0; rows <= 9; rows++)
+	{
+		for (columns = 1; columns <= 9; columns++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			times = rows * columns;
+			if (times <= 9)
+				_putchar(' ');
+			else
+				  _putchar((times / 10) + '0');
+
+			  _putchar((times % 10) + '0');
+		}
+		_putchar('\n');
+
+}}

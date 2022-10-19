1626f2040aa0ad7888debbcd1e07fff7459a4d4d
frankkib <frankkibagendi13@gmail.com>
2022-10-19 11:07:16 +0000

    printing ten minutes for jack everyday

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,14 +7,12 @@ void jack_bauer(void)
 {
 	int hrs, min;
 
-	hrs = 0;
-
 	for (hrs = 0; hrs <= 23; hrs++)
 	{
 		for (min = 0; min <= 59; min++)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
 			_putchar(':');
 			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');

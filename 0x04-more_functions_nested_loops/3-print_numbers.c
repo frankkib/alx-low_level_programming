e12065b24877842fa15bc3b91a421fb36be85214
frankkib <frankkibagendi13@gmail.com>
2022-10-21 12:10:23 +0000

    printing numbers 0 - 9 using putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - For printing numbers
+ * Returns: Always 0
+ */
+void print_numbers(void)
+{
+	char num = '0';
+
+	for (num = '0'; num < '9' num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
+

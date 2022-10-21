8fe0d3f1ccf45d802a1ac9f6d4ab0ccda9bfb84e
frankkib <frankkibagendi13@gmail.com>
2022-10-21 18:59:08 +0000

    printing and square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,7 +10,7 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (x = 0; x < size; ++)
+		for (x = 0; x < size; x++)
 		{
 			for (y = 0; y < (size - 1); y++)
 				_putchar('#');
@@ -19,4 +19,4 @@ void print_square(int size)
 		}
 	}
 	_putchar('\n');
-e
+}

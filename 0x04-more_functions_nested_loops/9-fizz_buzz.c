f7549a6509547dd06bccab7726108a99a9a74f06
frankkib <frankkibagendi13@gmail.com>
2022-10-21 19:22:30 +0000

    printing fizzbuzz message

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * Description: multiplies 3 print Fizz and 5 Buzz
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++;)
+	{
+		if (x % 15 == 0)
+			printf("FizzBuzz");
+		else if (x % 3 == 0)
+			printf("Fizz");
+		else if (x % 5 == 0)
+			printf("Buzz")
+		else
+			printf("%x", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}

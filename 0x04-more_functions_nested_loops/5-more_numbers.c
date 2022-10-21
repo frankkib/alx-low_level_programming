3a6993d7f9e452a2594c91b5b53d7673d1b6f006
frankkib <frankkibagendi13@gmail.com>
2022-10-21 12:55:46 +0000

    printing numbers 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,9 @@ void more_numbers(void)
 		{
 			if (j > 9)
 			{
-				_putchar((j / 10) + '0');
+				_putchar((j / 10) + '0' );
 			}
-			_putchar(j % 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar(10);
 	}

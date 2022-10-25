7d0545fa2a655d22ed3395930a11270c6036649b
frankkib <frankkibagendi13@gmail.com>
2022-10-25 12:50:54 +0000

    printing the length of a  a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+ * _strlen - prints the length of a string
+ * @s: the string
+ * Return: returns the length
+ */
+int _strlen(char *s)
+{
+	int len, count;
+
+	len = 0;
+	for (count = 0; s[count] != '\0'; count++)
+		len++
+			return (len);
+}

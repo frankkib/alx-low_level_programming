03a8813b2dbf43023dbf7863245cf261bcbb3667
frankkib <frankkibagendi13@gmail.com>
2022-10-28 07:57:30 +0000

    conticating a string form a source to a destination

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - it conticates two strings
+ * @dest: the destination of the string
+ * @src: string to be copied
+ * Return: the pointer dest
+ */
+char *_strcat(char *dest, char src)
+{
+	int n, m;
+
+	n = 0;
+
+	while (dest[n] != '\0')
+	{
+		n++;
+	}
+	m = 0;
+	while (src[m] != '\0')
+	{
+		destn[n] = src[m];
+		n++;
+		m++;
+
+	}
+	dest[n] = '\0';
+	return (dest);

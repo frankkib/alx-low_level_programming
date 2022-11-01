f9795ff330027f43e4f8426e1d9a7802cebda8da
frankkib <frankkibagendi13@gmail.com>
2022-11-01 13:14:43 +0000

    setting a value to a character

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - function sets a value to string
+ * @s: string to set from
+ * @to: the change string
+ * Return:0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

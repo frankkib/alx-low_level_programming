fb2916d7f4563b56552d84e812cec5e269215349
frankkib <frankkibagendi13@gmail.com>
2022-11-04 07:33:42 +0000

    printing the power of a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that returns the power of anumber
+ * @x: is the number
+ * @y: this is the power
+ * Return: the power @x to @y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+		return (x * (_power_recursion(x, y - 1)));
+}

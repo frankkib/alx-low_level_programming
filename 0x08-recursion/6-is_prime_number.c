6d65e7d70832cedb828bcf36bc5b63f559d6ba26
frankkib <frankkibagendi13@gmail.com>
2022-11-04 19:42:21 +0000

    finding prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * prime - function for finding prime numbers
+ * @i: number to be  checked
+ * @j: the number of times to be checked
+ * Return : prime number
+ */
+int prime(int i, int j)
+{
+	if (i <= 1 || i % j == 0)
+		return (0);
+	else if (i == j)
+		return (1);
+	else if (i > j)
+		prime(i, j + 1);
+	return (1);
+}
+/**
+ * is_prime_number - checks if the integer is prime
+ * @n: number to be checked
+ * Return: 1 if the input intege  otherwise return 0
+ */
+int is_prime_number(int n)
+{
+	return (prime(n, 2));
+}

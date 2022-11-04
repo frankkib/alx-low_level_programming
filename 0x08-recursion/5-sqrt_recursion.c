444344b95279db24788d7c7e2b14b1381f3b77c1
frankkib <frankkibagendi13@gmail.com>
2022-11-04 19:41:34 +0000

    finding the square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * sqfunction - the main entry point
+ * @n : ther number
+ * @p: the square of the number
+ * Return : -1 erroor
+ */
+int sqfunction(int n, int p)
+{
+	if ((p * p) == n)
+	{
+		return (p);
+	}
+	else
+	{
+		if ((p * p) > n)
+			return (-1);
+		else
+			return (sqfunction(n, p + 1));
+	}
+}
+/**
+ * _sqrt_recursion - function for finding a square root of a number
+ * @n: the number
+ * Return: square for n
+ */
+int _sqrt_recursion(int n)
+{
+	if
+	       	(n < 0);
+	{
+		return (-0);
+	}
+		return (sqfunction(n, 0));
+}

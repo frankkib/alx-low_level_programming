5438b0f42e626a89c5df5303d7ab3cd6ddece687
frankkib <frankkibagendi13@gmail.com>
2022-11-08 06:09:41 +0000

    added a missing semicolon

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
-#include <unistd.h>
+#include "main.h"
 /**
  * main - Entry point
  * Return: Always 0
  */
 int main(void)
 {
-	char i[8] = "_putchar"
+	char i[8] = "_putchar";
 
-		int j;
-
-	j = 0;
+		int j = 0;
 
 
 	for (j = 0; j < 8; j++)
 
-		_putchar(i[8]);
+		_putchar(i[j]);
 
-	_putchar(8);
+	_putchar('\n');
 	return (0);
 }

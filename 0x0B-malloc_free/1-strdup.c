ce12f688e6d7d7facc8b3c0d05288ae7b0db200a
frankkib <frankkibagendi13@gmail.com>
2022-11-09 12:55:19 +0000

    fuction to print newly allocated memory with a copy of the string given

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - function that prints newly allocated memory
+ * @str: the pointer string
+ * Return: NULL if str = NULL
+ */
+char *_strdup(char *str)
+{
+	char *num;
+	unsigned int i, j;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		num = malloc(sizeof(char) * (i + 1));
+		if (num == NULL)
+		{
+			return (NULL);
+		}
+	}
+	for (j = 0; j < i; j++)
+	{
+		num[i] = str[i];
+	}
+	num[i] = '\0';
+	return (num);
+}

7c06291140b963c308f95cc3ef585e049b5c05fc
frankkib <frankkibagendi13@gmail.com>
2022-11-09 13:11:20 +0000

    removed the 1 from malloc function

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		num = malloc(sizeof(char) * (i + 1));
+		num = malloc(sizeof(char) * (i++));
 		if (num == NULL)
 		{
 			return (NULL);

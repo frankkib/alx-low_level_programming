89701078c1c031b724ebfec1960a24257c561370
frankkib <frankkibagendi13@gmail.com>
2022-11-10 06:49:57 +0000

    function that prints the minimum coins making a change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,23 +17,23 @@ int main(int argc, char *argv[])
 		{
 			for ( ; cents >= 25; coins++)
 			{
-				cents -=  25;
+				cents = cents -  25;
 			}
 			for ( ; cents >= 10; coins++)
 			{
-				cents -= 10;
+				cents = cents - 10;
 			}
 			for ( ; cents >= 5; coins++)
 			{
-				cents -= 5;
+				cents = cents - 5;
 			}
 			for ( ; cents >= 2; coins++)
 			{
-				cents -=  2;
+				cents = cents -  2;
 			}
 			for ( ; cents >= 1; coins++)
 			{
-				cents -= 1;
+				cents = cents - 1;
 			}
 		}
 		printf("error\n");

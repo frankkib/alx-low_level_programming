beaba7aa4b4c6aad644b6d7e80ba0dd9a3c6fadb
frankkib <frankkibagendi13@gmail.com>
2022-11-10 06:41:22 +0000

    function that prints the minimum coins making a change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - functions that prints a change being given
+ * @argc: this is the argument count
+ * @argv: this is the argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int coins, cents = 0;
+
+	coins = 0;
+	if (argc == 2)
+	{
+		cents = atoi(argv[1]);
+		{
+			for ( ; cents >= 25; coins++)
+			{
+				cents = cents - 25;
+			}
+			for ( ; cents >= 10; coins++)
+			{
+				cents = cents - 10;
+			}
+			for ( ; cents >= 5; coins++)
+			{
+				cents = cents - 5;
+			}
+			for ( ; cents >= 2; coins++)
+			{
+				cents = cents - 2;
+			}
+			for ( ; cents >= 1; coins++)
+			{
+				cents = cents - 1;
+			}
+		}
+		printf("error\n");
+		return (0);
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+}

8f67ee2996cfd1bf8b821709072a4d7cf482c28d
frankkib <frankkibagendi13@gmail.com>
2022-11-15 10:15:41 +0000

    the dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - the structure of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+typedef struct dog dog_t;
+
+
+#endif

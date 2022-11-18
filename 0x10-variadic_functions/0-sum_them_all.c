3ab056975468bd8e2412cc6b9babedc4bc8b5bfd
frankkib <frankkibagendi13@gmail.com>
2022-11-18 10:32:18 +0000

    added the standard output header

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 /**
  *  sum_them_all - funtion that sums all the parameters.
  *  @n: the number of elements
  *  Return: 0
  */
-nt sum_them_all(const unsigned int n, ...)
+int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int sum, j;
 	va_list list;
@@ -16,7 +16,7 @@ nt sum_them_all(const unsigned int n, ...)
 	va_start(list, n);
 	for (j = 0; j < n; j++)
 	{
-		sum += va_list(list, int);
+		sum += va_arg(list, int);
 	}
 	va_end(list);
 	return (sum);

e79890680170fcbd57e3581336bef53a0f9d90b1
frankkib <frankkibagendi13@gmail.com>
2022-11-30 09:59:40 +0000

    added the number of elments

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,11 +8,14 @@ size_t print_listint(const listint_t *h)
 {
 	int number;
 
-	while (h != NULL)
+	if (h != NULL)
 	{
-		h = h->next;
-		number++;
-		printf("%d\n", h->n);
+		while (h->next != NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			number++;
+		}
 	}
 	return (number);
 }

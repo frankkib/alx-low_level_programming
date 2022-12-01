a243bfff44313359be5e39be306872e5e87eca96
frankkib <frankkibagendi13@gmail.com>
2022-12-01 08:53:22 +0000

    deleting a node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * int pop_listint - function that deletes a node
+ * @head: beginning of the node
+ * Return: the head node’s data (n).
+ */
+int pop_listint(listint_t **head)
+{
+	int n = 0;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (0);
+	temp = *head;
+	n = temp->n;
+	free(temp);
+	*head = temp->next;
+	return (n);
+}

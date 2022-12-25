c9f4dfc2f3f775bd8b3676e7f19ef8fc59f3173b
frankkib <frankkibagendi13@gmail.com>
2022-12-25 08:06:01 +0000

    missing brackects

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,12 @@ int main(void)
 	float b;
 	double c;
 	char d;
+	long long int e;
 
 	printf("long int size: %ld bytes\n", (sizeof(a)));
 	printf("float size: %ld bytes\n", (sizeof(b)));
 	printf("double size: %ld bytes\n", (sizeof(c)));
 	printf("char size: %ldbyte\n", (sizeof(d)));
+	 printf("long long int size: %ld bytes\n", (sizeof(e)));
 	return (0);
 }

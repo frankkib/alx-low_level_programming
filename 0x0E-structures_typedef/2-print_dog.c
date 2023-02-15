0a6d215d6ecf2549cdb8f458578596281e5938ea
frankkib <frankkibagendi13@gmail.com>
2023-02-15 13:23:01 +0000

    fixed the string error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,15 +12,15 @@ void print_dog(struct dog *d)
 		return;
 	}
 	if (d->name == NULL)
-		printf("Name:(nil)\n");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age < 0)
-		printf("Age:\n(nil)");
+		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("Owner:\n(nil)");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }

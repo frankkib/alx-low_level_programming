3cdbede2e7c700e5a1df7397fd0f1393762535a9
frankkib <frankkibagendi13@gmail.com>
2023-02-16 11:53:44 +0000

    added main header

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,18 +10,18 @@ char *_strstr(char *haystack, char *needle)
 	int num;
 
 	if (*needle == 0)
-		return;
+		return (haystack);
 	while (*haystack)
 	{
 		num = 0;
 
-		if (haystack[num] = needle[num])
+		if (haystack[num] == needle[num])
 		{
 			do {
 				if (needle[num + 1] == '\0')
 					return (haystack);
 				num++;
-			} while (haystack[num] == nenedle[num]);
+			} while (haystack[num] == needle[num]);
 		}
 		haystack++;
 	}

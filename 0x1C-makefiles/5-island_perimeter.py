784f73f641d1a720c7ed01b0d23520ccc21fa555
frankkib <frankkibagendi13@gmail.com>
2023-03-22 16:58:27 +0000

    function that returns the number perimeters

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class island_perimeter"""
+
+
+
+def island_perimeter(grid): 
+    """class representation"""
+  perimeter = 0
+  for row in range(len(grid)):
+    for col in range(len(grid[0])):
+      if grid[row][col] == 1:
+        if row - 1 < 0 or grid[row - 1][col] == 0:
+          perimeter += 1
+        if row + 1 >= len(grid) or grid[row + 1][col] == 0:
+          perimeter += 1
+        if col - 1 < 0 or grid[row][col - 1] == 0:
+          perimeter += 1
+        if col + 1 >= len(grid[0]) or grid[row][col + 1] == 0:
+          perimeter += 1
+  return perimeter

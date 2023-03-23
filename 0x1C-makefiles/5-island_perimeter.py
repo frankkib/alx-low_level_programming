bf011b26ea2828388d4f3ec8074cd28c7e8ea027
frankkib <frankkibagendi13@gmail.com>
2023-03-23 06:24:58 +0000

    fixed module documantaion

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,12 +12,16 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if grid[row][col] == 1:
+                """check if value is zero"""
                 if row - 1 < 0 or grid[row - 1][col] == 0:
                     perimeter += 1
+                    """check if value is zero"""
                     if row + 1 >= len(grid) or grid[row + 1][col] == 0:
                         perimeter += 1
+                        """check if value is zero"""
                         if col - 1 < 0 or grid[row][col - 1] == 0:
                             perimeter += 1
+                            """check if value is zero"""
                             if col + 1 >= len(grid[0]) or
                             grid[row][col + 1] == 0:
                                 perimeter += 1

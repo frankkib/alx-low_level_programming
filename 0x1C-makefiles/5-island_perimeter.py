8ab6fc179fbc654e5182827dafbe43f5c61ac893
frankkib <frankkibagendi13@gmail.com>
2023-03-23 06:11:40 +0000

    fixed module documantaion

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,19 +2,19 @@
 """class island_perimeter"""
 
 
-
-def island_perimeter(grid): 
+def island_perimeter(grid):
     """class representation"""
-  perimeter = 0
-  for row in range(len(grid)):
-    for col in range(len(grid[0])):
-      if grid[row][col] == 1:
-        if row - 1 < 0 or grid[row - 1][col] == 0:
-          perimeter += 1
-        if row + 1 >= len(grid) or grid[row + 1][col] == 0:
-          perimeter += 1
-        if col - 1 < 0 or grid[row][col - 1] == 0:
-          perimeter += 1
-        if col + 1 >= len(grid[0]) or grid[row][col + 1] == 0:
-          perimeter += 1
-  return perimeter
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                if row - 1 < 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                    if row + 1 >= len(grid) or grid[row + 1][col] == 0:
+                        perimeter += 1
+                        if col - 1 < 0 or grid[row][col - 1] == 0:
+                            perimeter += 1
+                            if col + 1 >= len(grid[0]) or
+                            grid[row][col + 1] == 0:
+                                perimeter += 1
+                                return perimeter

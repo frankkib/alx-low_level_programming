584d2c53880ce4c7a39bcea8ece685b4142e3440
frankkib <frankkibagendi13@gmail.com>
2023-05-09 09:58:42 +0000

    functiont that searches through an array of numbers

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * linear_search - function that searches through the array of numbers
+ * @array: the array being searched
+ * @sze: the size of the array
+ * @value: the value being search
+ * Return: Always 1 on success
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t j;
+	if (array == NULL){
+		return -1;
+	}
+	for (j = 0; j < size; j++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", j, array[j]);
+		if (array[j] == value) 
+		       return j;
+	}
+	return -1;
+}	
